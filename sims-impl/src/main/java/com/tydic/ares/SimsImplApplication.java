8efc2c23a3d0a10a22889470c4eec31a8acec725
xuebing <18362822048@163.com>
2019-05-23 14:33:39 +0800

    druid监控页面

--- sims-impl/src/main/java/com/tydic/ares/SimsImplApplication.java
+++ sims-impl/src/main/java/com/tydic/ares/SimsImplApplication.java
@@ -3,9 +3,7 @@ package com.tydic.ares;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 @EnableEurekaClient
